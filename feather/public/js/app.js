25b043c9295fb6bda300297ac082ae1e8e7e85bf
Sebastian Odell <sebastianodell@gmail.com>
2015-11-21 15:36:56 -0500

    ng-route working

--- feather/public/js/app.js
+++ feather/public/js/app.js
@@ -1,5 +1,19 @@
-var app = angular.module('FeatherApp', []);
+var app = angular.module('FeatherApp', ['ngRoute', 'ngAnimate']);
 
-app.controller('TestController', function(){
-	this.name = 'Sebastian';
+app.config(function($routeProvider){
+	$routeProvider.when('/', 
+		{
+			templateUrl: 	'partials/forecast.html',
+			controller: 	'WeatherCtrl',
+			controllerAs: 'weather'
+		})
+	});
+
+
+
+
+
+app.controller('WeatherCtrl', function(){
+	var self = this;
+	self.message = "Welcome to weather control"
 })
